061d382912d7c04ec41def7af675c779b7de8ae7
rnasifwhitestone <rnasifwhitestone@deloitte.co.uk>
2019-11-13 13:12:51 +0000

    Stage 1 changes

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GradeBook.GradeBooks
+{
+    using Enums;
+
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name)
+            : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
