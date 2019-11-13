1f0a39b62e84f9b6d3cbe136b44a3601f39e4b52
rnasifwhitestone <rnasifwhitestone@deloitte.co.uk>
2019-11-13 15:28:52 +0000

    Updated RankedGradeBook to return the letter grade properly

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -10,6 +12,26 @@ namespace GradeBook.GradeBooks
             : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+
+            IEnumerable<Student> students = Students.OrderByDescending(s => s.AverageGrade);
+
+            int fifth = Students.Count / 5;
+
+            if (averageGrade > students.Take(fifth).Last().AverageGrade)
+                return 'A';
+            if (averageGrade > students.Take(fifth * 2).Last().AverageGrade)
+                return 'B';
+            if (averageGrade > students.Take(fifth * 3).Last().AverageGrade)
+                return 'C';
+            if (averageGrade > students.Take(fifth * 4).Last().AverageGrade)
+                return 'D';
+            return 'F';
         }
     }
 }
